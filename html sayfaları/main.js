2154f41b73f110d7793bac02c8756944d422cef4
arihakan <73759052+salesin58@users.noreply.github.com>
2021-05-22 15:27:42 +0300

    fifth commit

--- html sayfaları/main.js
+++ html sayfaları/main.js
@@ -1,5 +1,5 @@
 var quotes=[];
-var colors=["#C62828","#AD1457","#4A148C","#311B92","#1A237E","#0D47A1","#01579B","#004D40","#1B5E20","#F9A825","#E65100","#263238"];
+
 
 
 function GetQuotes(){
@@ -19,8 +19,7 @@ function Change(){
     document.getElementById('text').innerText=quotes[rndNumb].text;
     document.getElementById('author').innerText="-"+quotes[rndNumb].author;
 
-    var rndColorNumb=Math.floor(Math.random()*12);
-    document.body.style.backgroundColor=colors[rndColorNumb];
+    
 }
 
 function ShareTweet(){
